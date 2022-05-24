13790264ff5a9a4b5228b014b0b32de48c598247
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2022-05-25 01:43:55 +0800

    Modify update-account id inside

--- admin/admin-panel/update-account.php
+++ admin/admin-panel/update-account.php
@@ -1,24 +1,15 @@
 <?php 
 	require('../admin-database.php');
 
-	if (isset($_POST['btnEdit'])) {
-		$editId = $_POST['editId'];
-		$editUsername = $_POST['editUsername'];
-		$editPassword = $_POST['editPassword'];
-        $editEmail = $_POST['email'];
-        $editType = $_POST['type'];
-		$editDisplayPhoto = $_POST['display_photo'];
-	}
-
 	if (isset($_POST['btnUpdate'])) {
-		$updateId = $_POST['updateId'];
-		$update_txtUsername = $_POST['update_txtUsername'];
-		$update_txtPassword = $_POST['update_txtPassword'];
-        $update_txtEmail = $_POST['update_email'];
-		$update_txtType = $_POST['update_type'];
-        $update_txtDisplayPhoto = $_POST['update_display_photo'];
+		$updateId = $_POST['edit-id'];
+		$update_txtUsername = $_POST['edit-username'];
+		$update_txtPassword = $_POST['edit-password'];
+        $update_txtEmail = $_POST['edit-email'];
+		$update_txtType = $_POST['edit-type'];
+        $update_txtDisplayPhoto = $_POST['edit-display-photo'];
 
-		mysqli_query($CON, "UPDATE tbladminaccs SET  username = '$update_txtUsername', password = '$update_txtPassword', email = '$update_txtEmail', type = '$update_txtType', display_photo = '$update_txtDisplayPhoto' WHERE idtbladminaccs = '$updateId';");
+		mysqli_query($CON, "UPDATE tbladminaccs SET username = '$update_txtUsername', password = '$update_txtPassword', email = '$update_txtEmail', type = '$update_txtType', display_photo = '$update_txtDisplayPhoto' WHERE idtbladminaccs = '$updateId';");
 
 		echo '<script> alert("Sucessfully Updated!") </script>';
 		echo '<script> window.location.href = "manage-accounts.php" </script>';
