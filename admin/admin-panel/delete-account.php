07856667fb955c493afadeb4ee3e1df32c453a2a
JBenedicto13 <jbenedicto20.jbb@gmail.com>
2022-05-24 23:01:58 +0800

    Add delete-account.php

---
+++ admin/admin-panel/delete-account.php
@@ -0,0 +1,14 @@
+<?php 
+	require('../admin-database.php');
+	if (isset($_POST['btnDelete'])) {
+
+		$txt_deleteId = $_POST['btnDeleteId'];
+
+		mysqli_query($CON, "DELETE FROM tbladminaccs WHERE idtbladminaccs = '$txt_deleteId';");
+
+		echo '<script> window.location.href = "manage-accounts.php" </script>';
+	}
+	else {
+		echo '<script> window.location.href = "manage-accounts.php" </script>';
+	}
+ ?>
